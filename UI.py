cc578cffcc3331004b6a1236dc607fba7f889948
Nguyễn Công Thọ <69784527+nguyen-tho@users.noreply.github.com>
2025-02-25 22:56:37 +0700

    update app, UI and google api. add openai api

--- UI.py
+++ UI.py
@@ -4,17 +4,26 @@ from llama_cpp import Llama
 from index_cpp import llama_chat
 import os
 
+import socket
+
+def is_connected():
+    """Checks if the device is connected to the internet."""
+    try:
+        # Try to resolve a common hostname (Google's public DNS)
+        socket.create_connection(("8.8.8.8", 53), timeout=5)  # Google DNS, port 53
+        return True
+    except OSError:
+        pass # Handle potential exceptions like timeouts, host not found, etc.
+    return False
+
 class LlamaChatUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Llama Chatbot")
 
-        # Bind Ctrl+S to save_conversation
-        self.root.bind('<Control-s>', self.save_conversation)
-        # Bind Alt+F4 to show_message
+      
+        # Bind Alt+F4 to quit
         self.root.bind('<Alt-F4>', self.quit)
-        # Bind Ctrl+O to open_conversation
-        self.root.bind('<Control-o>', self.open_conversation)
 
         # Create menu bar
         self.menu_bar = tk.Menu(root)
@@ -23,27 +32,69 @@ class LlamaChatUI:
         # Add file menu
         self.file_menu = tk.Menu(self.menu_bar, tearoff=0)
         self.menu_bar.add_cascade(label="File", menu=self.file_menu)
-        self.file_menu.add_command(label="Open Conversation", command=self.open_conversation)
-        self.file_menu.add_command(label="Save Conversation", command=self.save_conversation)
         self.file_menu.add_separator()
         self.file_menu.add_command(label="Quit", command=self.quit)
 
         
         self.answer_label = tk.Label(root, text="System's answer:")
         self.answer_label.grid(row=0, column=0, padx=10, pady=10)
-        self.answer_value = tk.Text(root, height=200, width=100)
+        self.answer_value = tk.Text(root, height=20, width=100)
         self.answer_value.grid(row=0, column=1,padx=10, pady=10)
         
+        self.mode_toggle = tk.Button(root,text='ONLINE MODE', bg='green', command=self.toggle_button) # default is online
+        self.mode_toggle.grid(row=1, column=0, padx=10, pady=10)
+        self.mode_toggle.is_on = True
+        
         self.prompt_label = tk.Label(root, text="Enter your prompt:")
-        self.prompt_label.grid(row=1, column=0, padx=10, pady=10)
+        self.prompt_label.grid(row=2, column=0, padx=10, pady=10)
         self.prompt_entry = tk.Entry(root, width=100)
-        self.prompt_entry.grid(row=1, column=1, padx=10, pady=10)
+        self.prompt_entry.grid(row=2, column=1, padx=10, pady=10)
+         # Insert placeholder text and set color
+        self.prompt_entry.insert(0, "Enter text here...")
+        self.prompt_entry.config(fg="grey")
+
+        # Bind events for placeholder functionality
+        self.prompt_entry.bind("<FocusIn>", self.on_entry_click)
+        self.prompt_entry.bind("<FocusOut>", self.on_focus_out)
+        
         self.prompt_btn = tk.Button(root, text='Enter', command=self.ask_question)
-        self.prompt_btn.grid(row=1, column=2, padx=10, pady=10)
+        self.prompt_btn.grid(row=2, column=2, padx=10, pady=10)
         
 
         # Add frame for output text and scrollbar
+    
+    
+    def on_entry_click(self, event):
+        if self.prompt_entry.get() == "Enter text here...":
+            self.prompt_entry.delete(0, tk.END)
+            self.prompt_entry.config(fg="black")  # Change text color
+
+    def on_focus_out(self, event):
+        if not self.prompt_entry.get():
+            self.prompt_entry.insert(0, "Enter text here...")
+            self.prompt_entry.config(fg="grey") # Change text color  
+            
+    def set_offline(self):
+        self.mode_toggle.config(text="OFFLINE MODE", bg="red")
+        self.mode_toggle.is_on = False
+
+    def set_online(self):
+        self.mode_toggle.config(text="ONLINE MODE", bg="green")
+        self.mode_toggle.is_on = True 
         
+    def toggle_button(self):
+        if self.mode_toggle.is_on:  # Currently offline
+            if not is_connected(): #check internet
+                messagebox.showerror(title='Connection Error!!', message="No Internet Connection. Please turn on the Internet or use offline mode")
+                self.set_offline()    
+            else:
+                self.set_offline() #also set offline if the button is currently online mode
+        else:  # Currently offline
+            if is_connected():
+                self.set_online()
+            else:
+                messagebox.showerror(title='Connection Error!!', message="No Internet Connection. Please turn on the Internet or use offline mode")
+                self.set_offline()
 
     def ask_question(self):
         prompt = self.prompt_entry.get()
@@ -54,47 +105,6 @@ class LlamaChatUI:
             self.answer_value.insert(tk.END, f"System: {answer_text}\n")
         except Exception as e:
             self.answer_value.insert(tk.END, f"Error occurred: {e}\n")
-
-    def save_conversation(self, event=None):
-        try:
-            # Define the specific directory to save the files
-            specific_directory = "conversation/"
-
-            # Ask for the file name without specifying the directory
-            file_path = filedialog.asksaveasfilename(defaultextension=".txt",
-                                                     filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
-
-            if file_path:
-                # Combine the specific directory with the file name
-                full_path = os.path.join(specific_directory, os.path.basename(file_path))
-
-                with open(full_path, "w") as file:
-                    conversation = self.output_text.get(1.0, tk.END)
-                    file.write(conversation)
-
-                messagebox.showinfo("Save Conversation", f"Conversation saved successfully to {full_path}!")
-        except Exception as e:
-            messagebox.showerror("Error", f"Failed to save conversation: {e}")
-
-    def open_conversation(self, event=None):
-        try:
-            specific_directory = "conversation/"
-
-            # Ask for the file name without specifying the directory
-            file_path = filedialog.asksaveasfilename(defaultextension=".txt",
-                                                     filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
-
-            if file_path:
-                # Combine the specific directory with the file name
-                full_path = os.path.join(specific_directory, os.path.basename(file_path))
-                
-                with open(full_path, "r") as file:
-                    conversation = file.read()
-                    self.output_text.delete(1.0, tk.END)  # Clear previous text
-                    self.output_text.insert(tk.END, conversation)
-                messagebox.showinfo("Open Conversation", "Conversation opened successfully!")
-        except Exception as e:
-            messagebox.showerror("Error", f"Failed to open conversation: {e}")
 
     def quit(self, event=None):
         messagebox.showinfo("Notice", "Are you sure to quit")
---
+++ open_ai_api.py
@@ -0,0 +1,4 @@
+from openai import OpenAI
+
+#Chat GPT and Deep Seek are use the same API
+
